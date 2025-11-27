c008f8fede04f076baf1f07238506bbe0f35114d
unknown <nichegolichnogo1717@gmail.com>
2025-11-27 14:16:58 +0300

    add js

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputLogin = document.querySelector(".authorization__login");
+const inputPassword = document.querySelector(".authorization__password");
+const autBtn = document.querySelector(".btn__auth");
+
+function validFormInput() {
+  if (inputLogin.value === "" || inputPassword.value === "") {
+    alert("Заполните форму");
+    return false;
+  }
+  if (inputPassword.value.length < 6) {
+    alert("Пароль должен содержать минимум 6 символов");
+  } else {
+    alert(`${inputLogin.value}, Добро пожаловать`);
+    inputLogin.value === "";
+    inputPassword.value === "";
+  }
+}
+
+autBtn.addEventListener("click", validFormInput);
