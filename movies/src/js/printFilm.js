dbeb3ccab458bad552f04d45b04481273b6b9b47
unknown <nichegolichnogo1717@gmail.com>
2026-03-06 01:55:59 +0300

    init

---
+++ movies/src/js/printFilm.js
@@ -0,0 +1,13 @@
+import { btnAddFilm, closeModal, modalInput } from "./modal.js";
+const listFilm = document.querySelector(".movie-list");
+
+console.log(btnAddFilm);
+function printFilm() {
+  const film = document.createElement("li");
+  film.textContent = modalInput.value;
+  listFilm.appendChild(film);
+
+  closeModal();
+}
+btnAddFilm.addEventListener("click", printFilm);
+export { printFilm };
